3bb48fe8a51d7a4680f8eaf7d967025e01edf975
queonias <queones@gmail.com>
2023-07-05 23:18:14 -0300

    feat: Card components at home

---
+++ pages/components/Card.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../../styles/Card.module.css"
+
+
+export default function Card({pokemon}) {
+
+    function formatNumber(num) {
+        if (num < 10) {
+          return "00" + num;
+        } else if (num < 100) {
+          return "0" + num;
+        } else {
+          return num;
+        }
+      }
+      
+
+    return(
+     <div className={ styles.card }>
+        <Image 
+        src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${formatNumber(pokemon.id)}.png`} 
+        alt="pokemon" 
+        width="120" 
+        height="120"
+        />
+        <p className={ styles.id }>#{pokemon.id}</p>
+        <h3 className={ styles.title }>{pokemon.name}</h3>
+        <Link className={ styles.btn } href={`/pokemon/${pokemon.id}`}>Detalhes</Link>
+     </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Home.module.css';
 import Image from 'next/image';
+import Card from './components/Card';
 
 export async function getStaticProps() {
 
-  const maxPokemons = 251;
+  const maxPokemons = 1000;
   const api = 'https://pokeapi.co/api/v2/pokemon/';
   const res = await fetch(`${api}/?limit=${maxPokemons}`);
   const data = await res.json();
@@ -29,7 +30,7 @@ export default function Home({ pokemons }) {
       </div>
       <div className={ styles.pokemon_container}>
           {pokemons.map((pokemon) => (
-            <p key={pokemon.id}>{pokemon.name}</p>
+            <Card key={pokemon.id} pokemon={pokemon} />
           ))}
       </div>
     </>
